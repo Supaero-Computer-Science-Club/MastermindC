f90deea14b56053a81c2c1c5fc8984ef1192bf49
g.brivady <gregory.brivady@gmail.com>
2021-10-08 14:09:30 +0200

    [untested] implemented code2number functions and its inverse

--- inc/tools.h
+++ inc/tools.h
@@ -1,4 +1,10 @@
 #ifndef TOOLS_H
 #define TOOLS_H
+#include <stdlib.h>
+#include <stdio.h>
+
+int* code_from_number(long int num, int code_length, int colors);
+
+long int number_from_code(int* p_code, int code_length, int colors);
 
 #endif
--- src/tools.c
+++ src/tools.c
@@ -1,1 +1,31 @@
 #import "tools.h"
+
+int* code_from_number(long int num, int code_length, int colors){
+
+    int* p_code = malloc(sizeof(int)*code_length);
+    if(p_code == NULL){
+        printf("Error : cannot allocate memory for code in code_from_number");
+    }
+    long int a = num;
+    int b = 0;
+    for(int i = 0; i < code_length, i++){
+        b = (int) num % colors;
+        if(b < 0){
+            b += colors;
+        }
+        a = num/colors;
+        p_code[code_length - 1 - i] = b;
+    }
+
+    return p_code;
+}
+
+long int number_from_code(int* code, int code_length, int colors){
+    long int num = 0;
+    long int exp = 1;
+    for(i = 0; i < code_length; i++){
+        num += exp * code[code_length - 1 - i];
+        exp *= colors;
+    }
+    return num;
+}
